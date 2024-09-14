e153827c531f24c38c04343cd09b39b7b76b83a2
emon5369 <emon5369@gmail.com>
2024-09-14 18:28:57 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ function App() {
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
       <div className="bg-[#172842] min-h-screen py-8 flex justify-center">
-        <div className="md:border border-slate-600 px-1 lg:w-1/2">
+        <div className="md:border border-slate-600 px-1 w-full lg:w-1/2">
           <div className="w-full mx-auto shadow-md rounded-lg px-4 py-3 text-white">
             <h1 className="text-3xl font-serif font-bold text-center text-yellow-500 mb-8 mt-2">
               Todo App
