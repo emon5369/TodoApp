262abf8f9cc345a2ffa29876012d5675446c7e46
emon5369 <emon5369@gmail.com>
2024-09-14 18:24:49 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -42,14 +42,11 @@ function App() {
     localStorage.setItem("todos", JSON.stringify(todos))
   }, [todos])
 
-
-
-
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
       <div className="bg-[#172842] min-h-screen py-8 flex justify-center">
-        <div className="md:border border-slate-600 px-1">
-          <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+        <div className="md:border border-slate-600 px-1 lg:w-1/2">
+          <div className="w-full mx-auto shadow-md rounded-lg px-4 py-3 text-white">
             <h1 className="text-3xl font-serif font-bold text-center text-yellow-500 mb-8 mt-2">
               Todo App
             </h1>
@@ -58,9 +55,7 @@ function App() {
             </div>
             <div className="flex flex-wrap gap-y-3">
               {todos.map((todo) => (
-                <div key={todo.id}
-                  className='w-full'
-                >
+                <div key={todo.id} className='w-full'>
                   <TodoItem todo={todo} />
                 </div>
               ))}
